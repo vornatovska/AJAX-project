e24ca3dd3d4d6d55a0e34b7278b23e8f85667e56
vitaornat <vornatovska@gmail.com>
2017-04-28 12:51:42 +0300

    with post request and validation

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-function load(apiKeyU, idU) {
-    var apiKey = document.getElementById(apiKeyU).value;
-    var profil = document.getElementById(idU).value;
+function load() {
+    
+    var apiKey = document.getElementById("apiKeyUser").value;
+    var profil = document.getElementById("idUser").value;
 
+
+    var request = document.getElementsByName('request');
+    var requestMetod = "GET";
+
+    if(validate("apiKeyUser", "Api key")==false || validate("idUser", "Profil-ID")==false){
+        return false;
+    }
+
+
+    function validate(field, fieldUser) {
+        var valid = true;
+        if(document.getElementById(field).value == ""){
+            valid = false;
+            alert("Input value for field " + fieldUser);
+        }
+        return valid;
+    }
+
+
+    for (var i =0 ; i < request.length; i++ ){
+        if (request[i].checked && request[i].value == 0) {
+            break;
+        }else{
+            requestMetod = "POST";
+            break;
+        }
+    }
 
     var ajaxObject;
 
@@ -11,21 +39,29 @@ function load(apiKeyU, idU) {
         ajaxObject = new ActiveXObject("Microsoft.XMLHTTP");
     }
 
-    ajaxObject.onreadystatechange = function() { alertContents(ajaxObject); };
+    if(requestMetod == "GET"){
+        ajaxObject.onreadystatechange = function() { alertContents(ajaxObject); };
+        var params = 'apiKey=' + apiKey +
+            '&profil=' + profil;
+        ajaxObject.open("GET",'/api/v1/inputRecord?' + params,true);
+        ajaxObject.send();
+    } else {
+        ajaxObject.onreadystatechange = function() { alertContents(ajaxObject); };
+        var params = 'apiKey=' + apiKey +
+            '&profil=' + profil;
+        ajaxObject.open("POST",'/api/v1/inputRecord',true);
+        ajaxObject.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+        ajaxObject.send("params");
+    }
 
-
-    var params = 'apiKey=' + apiKey +
-        '&profil=' + profil;
-    ajaxObject.open("GET",'inputRecord?' + params,true);
-    ajaxObject.send();
 
     function alertContents(httpRequest) {
 
         if (httpRequest.readyState == 4) {
             if (httpRequest.status == 200) {
-                alert(httpRequest.responseText);
+                httpRequest.responseText;
             } else {
-                alert('Request error');
+                //alert('Request error');
             }
         }
     }
